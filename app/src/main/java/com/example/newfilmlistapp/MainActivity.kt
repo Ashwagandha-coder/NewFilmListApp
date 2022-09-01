160c358808557515c18f7db0d465d870a4af0a43
Ashawagandha-coder <max.cacz@yandex.ru>
2022-09-01 12:51:51 +0500

    add yet dependencies

--- app/src/main/java/com/example/newfilmlistapp/MainActivity.kt
+++ app/src/main/java/com/example/newfilmlistapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.newfilmlistapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    fun doSome() {
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+
+
+
+    }
+
+
+
 }
