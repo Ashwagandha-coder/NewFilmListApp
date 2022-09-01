204f0b62bf34ea72b97c3b0b908e14eb046b4ec7
Ashawagandha-coder <max.cacz@yandex.ru>
2022-09-01 16:23:11 +0500

    add get method in interface

--- app/src/main/java/com/example/newfilmlistapp/MainActivity.kt
+++ app/src/main/java/com/example/newfilmlistapp/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.newfilmlistapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import retrofit2.Retrofit
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,17 +11,6 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-//    fun doSome() {
-//
-//        val retrofit = Retrofit.Builder()
-//            .baseUrl("https://api.github.com/")
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .build()
-//
-//
-//
-//
-//    }
 
 
 
