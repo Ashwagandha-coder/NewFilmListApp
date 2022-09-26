095a5236d657b2a6b128f70cab443236202cfd58
Ashawagandha-coder <max.cacz@yandex.ru>
2022-09-26 13:51:39 +0500

    add logic in back fragment

--- app/src/main/java/com/example/newfilmlistapp/ui/MovieDetail.kt
+++ app/src/main/java/com/example/newfilmlistapp/ui/MovieDetail.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.newfilmlistapp.databinding.FragmentMovieDetailBinding
 
 
@@ -19,11 +20,26 @@ class MovieDetail : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMovieDetailBinding.inflate(inflater,container,false)
+
+        onBackScreen()
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    fun onBackScreen() {
+
+        binding.topNavBar.returnImageView.setOnClickListener {
+
+            val action = MovieDetailDirections.actionMovieDetailToRandom()
+            it.findNavController().navigate(action)
+
+        }
+
+
     }
 
 
