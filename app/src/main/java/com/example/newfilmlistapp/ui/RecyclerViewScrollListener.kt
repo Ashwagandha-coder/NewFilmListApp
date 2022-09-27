0deeece273a078e81875d6c9b7244f01c0abb872
Ashawagandha-coder <max.cacz@yandex.ru>
2022-09-27 16:58:49 +0500

    add hiretance in custom recycler view

--- app/src/main/java/com/example/newfilmlistapp/ui/RecyclerViewScrollListener.kt
+++ app/src/main/java/com/example/newfilmlistapp/ui/RecyclerViewScrollListener.kt
@@ -1,12 +1,24 @@
 package com.example.newfilmlistapp.ui
 
-class RecyclerViewScrollListener {
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerViewScrollListener() : RecyclerView.OnScrollListener() {
 
     private var loading: Boolean = true
 
-    private var totalItemCount: Int = 0
+    private var firstVisibleItem: Int = 0
     private var visibleItemCount: Int = 0
-    private var firstVisibleCount: Int = 0
+    private var totalItemCount: Int = 0
+    private var visibleThreshold: Int = 10
+    private var previousTotal: Int = 0
+
+    override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+        super.onScrollStateChanged(recyclerView, newState)
+    }
+
+    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+        super.onScrolled(recyclerView, dx, dy)
+    }
 
 
 }
