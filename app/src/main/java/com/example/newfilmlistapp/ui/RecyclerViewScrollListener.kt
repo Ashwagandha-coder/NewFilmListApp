97872170847ff76cf383ba0b9c1f682f18f29318
Ashawagandha-coder <max.cacz@yandex.ru>
2022-09-27 16:54:20 +0500

    add field in recyclerView

--- app/src/main/java/com/example/newfilmlistapp/ui/RecyclerViewScrollListener.kt
+++ app/src/main/java/com/example/newfilmlistapp/ui/RecyclerViewScrollListener.kt
@@ -1,4 +1,12 @@
 package com.example.newfilmlistapp.ui
 
 class RecyclerViewScrollListener {
+
+    private var loading: Boolean = true
+
+    private var totalItemCount: Int = 0
+    private var visibleItemCount: Int = 0
+    private var firstVisibleCount: Int = 0
+
+
 }
