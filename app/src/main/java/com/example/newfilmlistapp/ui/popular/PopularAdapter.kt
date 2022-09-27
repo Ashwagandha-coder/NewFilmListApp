8f237cc46ce189fc100be9e4ca995f99ff191b34
Ashawagandha-coder <max.cacz@yandex.ru>
2022-09-27 17:58:51 +0500

    implements method in adapter for recycler

--- app/src/main/java/com/example/newfilmlistapp/ui/popular/PopularAdapter.kt
+++ app/src/main/java/com/example/newfilmlistapp/ui/popular/PopularAdapter.kt
@@ -1,11 +1,24 @@
 package com.example.newfilmlistapp.ui.popular
 
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.example.newfilmlistapp.model.ResultPopular
 
-class PopularAdapter() {
 
-    init {
+class PopularAdapter() : ListAdapter<ResultPopular, PopularViewHolder>() {
+
+    // todo: Сделать логику скроллинга recycler view
+
+    override fun getItemCount(): Int {
+        return super.getItemCount()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PopularViewHolder {
+
+
+    }
+
+    override fun onBindViewHolder(holder: PopularViewHolder, position: Int) {
 
     }
 
