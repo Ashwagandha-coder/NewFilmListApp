c588b8563a1c8aedb35ad08f7dac32b7d2d1b4d5
Ashawagandha-coder <max.cacz@yandex.ru>
2022-09-27 15:58:36 +0500

    save state before launch

--- app/src/main/java/com/example/newfilmlistapp/ViewModel/ViewModel_Popular.kt
+++ app/src/main/java/com/example/newfilmlistapp/ViewModel/ViewModel_Popular.kt
@@ -1,21 +1,49 @@
 package com.example.newfilmlistapp.ViewModel
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.newfilmlistapp.model.PopularWrapper
+import com.example.newfilmlistapp.network.LoadingMovieDBService
+import com.example.newfilmlistapp.network.Retrofit
+import kotlinx.coroutines.launch
 
 class ViewModel_Popular: ViewModel() {
 
+
+    private val mutableLiveData_popularMovie: MutableLiveData<PopularWrapper> = MutableLiveData()
+    val popularMovie = mutableLiveData_popularMovie
+
     fun requestPopular() {
 
+        viewModelScope.launch {
 
+            try {
 
+                mutableLiveData_popularMovie.value = getPopularMovie()!!
 
+            }
+
+            catch (e: Exception) {
+                Log.d(ViewModel_Popular::class.java.name,"request Error - Popular Movie")
+                e.printStackTrace()
+            }
+
+        }
 
 
     }
 
-    suspend fun getPopularMovie() {
+    suspend fun getPopularMovie(): PopularWrapper {
 
+        val loadingMovieDBService = Retrofit.retrofit.create(LoadingMovieDBService::class.java)
 
+        val result = loadingMovieDBService.getPopularMovie()
+
+        Log.d(ViewModel_Popular::class.java.name,"request OK - Popular Movie")
+
+        return result
 
     }
 
--- app/src/main/java/com/example/newfilmlistapp/ui/Popular.kt
+++ app/src/main/java/com/example/newfilmlistapp/ui/Popular.kt
@@ -5,6 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import com.example.newfilmlistapp.ViewModel.ViewModel_Popular
+import com.example.newfilmlistapp.ViewModel.ViewModel_SortByDate
 import com.example.newfilmlistapp.databinding.FragmentPopularBinding
 
 
@@ -12,16 +16,34 @@ class Popular : Fragment() {
 
     private lateinit var binding: FragmentPopularBinding
 
+    private val viewModel: ViewModel_Popular by lazy {
+        ViewModelProvider(this).get(ViewModel_Popular::class.java)
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         binding = FragmentPopularBinding.inflate(inflater,container,false)
 
+
+        viewModel.requestPopular()
+
         return binding.root
+    }
+
+
+    fun workWithViewModel() {
+
+        viewModel.popularMovie.observe(viewLifecycleOwner) {
+
+
+
+        }
+
+
     }
 
 
