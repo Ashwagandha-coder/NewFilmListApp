44dfd21979046a558edf8ff31457016148cf3062
Ashawagandha-coder <max.cacz@yandex.ru>
2022-09-27 18:31:15 +0500

    add logic in bindViewHolder and add in Constant base url for picture

--- app/src/main/java/com/example/newfilmlistapp/ui/popular/PopularAdapter.kt
+++ app/src/main/java/com/example/newfilmlistapp/ui/popular/PopularAdapter.kt
@@ -3,6 +3,9 @@ package com.example.newfilmlistapp.ui.popular
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.newfilmlistapp.BASE_URL_FOR_PICTURE
 import com.example.newfilmlistapp.R
 import com.example.newfilmlistapp.model.ResultPopular
 import com.example.newfilmlistapp.ui.UserDiffCallBack
@@ -23,7 +26,30 @@ class PopularAdapter() : ListAdapter<ResultPopular, PopularViewHolder>(UserDiffC
 
     }
 
-    override fun onBindViewHolder(holder: PopularViewHolder, position: Int) {
+    override fun onBindViewHolder(viewHolder: PopularViewHolder, position: Int) {
+
+         val movie: ResultPopular = getItem(position)
+        val date = movie.releaseDate
+
+
+        viewHolder.movieName.also {
+            it.text = movie.title
+        }
+
+        viewHolder.movieOverview.also {
+            it.text = movie.overview
+        }
+
+        viewHolder.movieReleaseDate.also {
+            it.text = date
+        }
+
+        if (movie.posterPath != null) {
+            Glide.with(viewHolder.posterPath.context)
+                .load(BASE_URL_FOR_PICTURE + movie.posterPath)
+                .apply(RequestOptions().centerCrop())
+                .into(viewHolder.posterPath)
+        }
 
     }
 
