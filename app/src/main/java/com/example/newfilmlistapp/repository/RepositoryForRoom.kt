ec435e4b9de36abd2548cc867e73b8801a711f0e
Ashawagandha-coder <max.cacz@yandex.ru>
2022-09-30 17:02:11 +0500

    add new method in RepositoryForRoom.kt

--- app/src/main/java/com/example/newfilmlistapp/repository/RepositoryForRoom.kt
+++ app/src/main/java/com/example/newfilmlistapp/repository/RepositoryForRoom.kt
@@ -1,8 +1,26 @@
 package com.example.newfilmlistapp.repository
 
+import com.example.newfilmlistapp.model.ResultPopular
+
 interface RepositoryForRoom {
 
+    suspend fun insertDB()
 
+    suspend fun updateDB()
+
+    suspend fun getMovieListLocal(): List<ResultPopular>?
+
+    suspend fun getMovieLocal(id: String): ResultPopular?
+
+    suspend fun insertLocal(movie: ResultPopular)
+
+    suspend fun insertLocal(list: List<ResultPopular>)
+
+    suspend fun updateLocal(movie: ResultPopular)
+
+    suspend fun deleteMovieLocal(movie: ResultPopular)
+
+    suspend fun deleteMovieLocal(id: String)
 
 
 }
