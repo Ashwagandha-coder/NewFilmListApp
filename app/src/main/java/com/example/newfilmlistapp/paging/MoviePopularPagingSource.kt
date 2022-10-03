18ea3843e1116f7dcdb7e7aa0bd8c9c4ee3122dc
Ashawagandha-coder <max.cacz@yandex.ru>
2022-10-03 17:16:46 +0500

    add base logic in pagination

--- app/src/main/java/com/example/newfilmlistapp/paging/MoviePopularPagingSource.kt
+++ app/src/main/java/com/example/newfilmlistapp/paging/MoviePopularPagingSource.kt
@@ -3,19 +3,34 @@ package com.example.newfilmlistapp.paging
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.example.newfilmlistapp.model.PopularWrapper
+import com.example.newfilmlistapp.model.ResultPopular
+import com.example.newfilmlistapp.view_model.ViewModel_Popular
 
-class MoviePopularPagingSource(): PagingSource<Int, PopularWrapper>() {
+class MoviePopularPagingSource(): PagingSource<Int, ResultPopular>() {
 
 
-    override fun getRefreshKey(state: PagingState<Int, PopularWrapper>): Int? {
+    override fun getRefreshKey(state: PagingState<Int, ResultPopular>): Int? {
 
         return null
 
     }
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, PopularWrapper> {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, ResultPopular> {
 
 
+        return try {
+
+            val currentPage = params.key ?: 1
+            val data = ViewModel_Popular().getPopularMovie().results ?: emptyList()
+
+            LoadResult.Page(data = data,
+                prevKey = if (currentPage == 1) null else -1,
+                nextKey = currentPage.plus(1)
+            )
+        }
+        catch (e: Exception) {
+            LoadResult.Error(e)
+        }
 
     }
 }
