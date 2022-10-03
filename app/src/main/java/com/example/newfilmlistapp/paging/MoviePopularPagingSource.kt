ae1493542a1bdb66f3bfb82ff031959415d4e153
Ashawagandha-coder <max.cacz@yandex.ru>
2022-10-03 16:58:58 +0500

    override base method pagination and test suspend fun in view model

--- app/src/main/java/com/example/newfilmlistapp/paging/MoviePopularPagingSource.kt
+++ app/src/main/java/com/example/newfilmlistapp/paging/MoviePopularPagingSource.kt
@@ -1,6 +1,21 @@
 package com.example.newfilmlistapp.paging
 
-class MoviePopularPagingSource {
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.newfilmlistapp.model.PopularWrapper
+
+class MoviePopularPagingSource(): PagingSource<Int, PopularWrapper>() {
 
 
+    override fun getRefreshKey(state: PagingState<Int, PopularWrapper>): Int? {
+
+        return null
+
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, PopularWrapper> {
+
+
+
+    }
 }
--- app/src/main/java/com/example/newfilmlistapp/view_model/ViewModel_Popular.kt
+++ app/src/main/java/com/example/newfilmlistapp/view_model/ViewModel_Popular.kt
@@ -4,9 +4,12 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
 import com.example.newfilmlistapp.model.PopularWrapper
 import com.example.newfilmlistapp.network.LoadingMovieDBService
 import com.example.newfilmlistapp.network.Retrofit
+import com.example.newfilmlistapp.paging.MoviePopularPagingSource
 import kotlinx.coroutines.launch
 
 class ViewModel_Popular: ViewModel() {
@@ -45,6 +48,18 @@ class ViewModel_Popular: ViewModel() {
         Log.d(ViewModel_Popular::class.java.name,"request OK - Popular Movie")
 
         return result
+
+    }
+
+    suspend fun testGetPopularMovie() {
+
+        val popularMovie = Pager(PagingConfig(pageSize = 1)) {
+
+            MoviePopularPagingSource()
+
+        }
+
+
 
     }
 
