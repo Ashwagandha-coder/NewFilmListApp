cde95965efe8014b5526231db5ae766fa3c0aded
Ashawagandha-coder <max.cacz@yandex.ru>
2022-10-07 15:26:02 +0500

    delete old code

--- app/src/main/java/com/example/newfilmlistapp/view_model/MovieRecomendationViewModel.kt
+++ app/src/main/java/com/example/newfilmlistapp/view_model/MovieRecomendationViewModel.kt
@@ -6,7 +6,6 @@ import com.example.newfilmlistapp.model.GenresWrapper
 import com.example.newfilmlistapp.model.MovieWrapper
 import com.example.newfilmlistapp.network.LoadingMovieDBService
 import kotlinx.coroutines.launch
-import kotlin.random.Random
 
 class MovieRecomendationViewModel : ViewModel(), ViewModelProvider.Factory {
 
@@ -21,15 +20,6 @@ class MovieRecomendationViewModel : ViewModel(), ViewModelProvider.Factory {
     private val mutableLiveData_default_movie: MutableLiveData<MovieWrapper> = MutableLiveData()
     val default_movie: LiveData<MovieWrapper> = mutableLiveData_default_movie
 
-//    private val mutableLiveData_saveData: MutableLiveData<SaveDataBackScreen> = MutableLiveData()
-//    val saveData: LiveData<SaveDataBackScreen> = mutableLiveData_saveData
-
-    private lateinit var genresWrapper: GenresWrapper
-
-    private lateinit var movie_ID: String
-
-    private var number_random: Int = 0
-    val array_index = number_random
 
 
     fun requestGenres() {
@@ -64,17 +54,6 @@ class MovieRecomendationViewModel : ViewModel(), ViewModelProvider.Factory {
 
                 mutableLiveData_default_movie.value = variable
 
-                number_random = Random.Default.nextInt(0,9)
-
-                Log.d(MovieRecomendationViewModel::class.java.name,"Random number - $number_random")
-
-                movie_ID = variable.results.get(number_random)?.id.toString() ?: ""
-
-                Log.d(MovieRecomendationViewModel::class.java.name,movie_ID + " " + "Значение movie_ID")
-
-
-//                mutableLiveData_saveData.value = SaveDataBackScreen(year,index,variable.results.get(index).posterPath,variable)
-
             }
             catch (e: Exception) {
                 Log.d(MovieRecomendationViewModel::class.java.name,"Error Request - Default Movie")
@@ -96,20 +75,10 @@ class MovieRecomendationViewModel : ViewModel(), ViewModelProvider.Factory {
 
                 Log.d(MovieRecomendationViewModel::class.java.name,"year " + year.javaClass + " " + "genre " + genre.javaClass + " " + "- In Request Movie")
 
-                val variable = getMovie(year, genre)!!
+                val variable = getMovie(year, genre)
 
                 mutableLiveData_movie.value = variable
 
-                number_random = Random.Default.nextInt(0,9)
-
-                Log.d(MovieRecomendationViewModel::class.java.name,"Random number - $number_random")
-
-                movie_ID = variable.results.get(number_random)?.id.toString() ?: ""
-
-                Log.d(MovieRecomendationViewModel::class.java.name,movie_ID + " " + "Значение movie_ID")
-
-
-//                mutableLiveData_saveData.value = SaveDataBackScreen(year,index,variable.results.get(index).posterPath,variable)
 
             }
             catch (e: Exception) {
@@ -127,7 +96,7 @@ class MovieRecomendationViewModel : ViewModel(), ViewModelProvider.Factory {
 
         val loadingMovieDBService = com.example.newfilmlistapp.network.Retrofit.retrofit.create(LoadingMovieDBService::class.java)
 
-        val result = loadingMovieDBService.getMovie(primary_release_year = year, genres = listOf<String>(genre))
+        val result = loadingMovieDBService.getMovie(primary_release_year = year, genres = listOf(genre))
 
         Log.d(MovieRecomendationViewModel::class.java.name,"request OK - Random Movie ")
 
