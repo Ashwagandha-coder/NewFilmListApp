348c0b5f1c95251ae029f1ee93cc419f2b30be7a
Ashawagandha-coder <max.cacz@yandex.ru>
2022-10-10 18:27:52 +0500

    add live data in viewModel favorites

--- app/src/main/java/com/example/newfilmlistapp/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/newfilmlistapp/ui/favorites/FavoritesFragment.kt
@@ -4,10 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.example.newfilmlistapp.databinding.FragmentFavoritesBinding
+import com.example.newfilmlistapp.databinding.FragmentPopularBinding
+import com.example.newfilmlistapp.model.ResultPopular
+import com.example.newfilmlistapp.ui.popular.PopularAdapter
+import com.example.newfilmlistapp.view_model.PopularViewModel
 
 
 class FavoritesFragment : androidx.fragment.app.Fragment() {
+
+    private var allMovies = arrayListOf<ResultPopular>()
+    private var totalResults: Int = -1
+    private var isLoading: Boolean = false
+
+    private val viewModel: PopularViewModel by lazy {
+        ViewModelProvider(this).get(PopularViewModel::class.java)
+    }
+
+    private val favoriteAdapter: PopularAdapter = PopularAdapter()
+    //private val mScrollListener by lazy { RecyclerViewScrollListener(this) }
+
+
 
     private lateinit var binding: FragmentFavoritesBinding
 
--- app/src/main/java/com/example/newfilmlistapp/view_model/FavoritesViewModel.kt
+++ app/src/main/java/com/example/newfilmlistapp/view_model/FavoritesViewModel.kt
@@ -4,13 +4,14 @@ import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.newfilmlistapp.local.db.Room
 import com.example.newfilmlistapp.model.MovieDetailWrapper
 import kotlinx.coroutines.launch
 
 
 class FavoritesViewModel() : ViewModel() {
 
-    private val mutableLiveData_favorite: MutableLiveData<MovieDetailWrapper> = MutableLiveData()
+    private val mutableLiveData_favorite: MutableLiveData<List<MovieDetailWrapper>> = MutableLiveData()
     val favorite = mutableLiveData_favorite
 
 
@@ -20,6 +21,7 @@ class FavoritesViewModel() : ViewModel() {
 
             try {
 
+                mutableLiveData_favorite.value = Room.room.movieDao().getMovieList()
 
 
             }
