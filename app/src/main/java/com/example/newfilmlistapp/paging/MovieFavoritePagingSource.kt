d2feb0c825e74ffece830410f03049e24bf46f29
Ashawagandha-coder <max.cacz@yandex.ru>
2022-10-11 12:59:18 +0500

    impl abstract class paging source

--- app/src/main/java/com/example/newfilmlistapp/paging/MovieFavoritePagingSource.kt
+++ app/src/main/java/com/example/newfilmlistapp/paging/MovieFavoritePagingSource.kt
@@ -1,11 +1,22 @@
 package com.example.newfilmlistapp.paging
 
-class MovieFavoritePagingSource {
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.newfilmlistapp.model.MovieDetailWrapper
+
+class MovieFavoritePagingSource: PagingSource<Int,MovieDetailWrapper>() {
 
 
+    override fun getRefreshKey(state: PagingState<Int, MovieDetailWrapper>): Int? {
 
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
 
+    }
 
-
-
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, MovieDetailWrapper> {
+        TODO("Not yet implemented")
+    }
 }
