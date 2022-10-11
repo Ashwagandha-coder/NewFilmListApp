d973aa610b8113f2d54b15c02630b2a58b97eea1
Ashawagandha-coder <max.cacz@yandex.ru>
2022-10-11 15:34:56 +0500

    refactoring model for network and create new model for room

--- app/src/main/java/com/example/newfilmlistapp/model/MovieDetailWrapper.kt
+++ app/src/main/java/com/example/newfilmlistapp/model/MovieDetailWrapper.kt
@@ -1,16 +1,14 @@
 package com.example.newfilmlistapp.model
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 
-@Entity(tableName = "movie")
+
 @JsonClass(generateAdapter = true)
 data class MovieDetailWrapper (
-    @PrimaryKey(autoGenerate = false)
-    val adult: Boolean? = null,
+
+    val adult: Boolean,
 
     @Json(name = "backdrop_path")
     val backdropPath: String? = null,
@@ -18,66 +16,66 @@ data class MovieDetailWrapper (
     @Json(name = "belongs_to_collection")
     val belongsToCollection: Any? = null,
 
-    val budget: Long? = null,
-    val genres: List<Genres>? = null,
+    val budget: Long?,
+    val genres: List<Genres>,
     val homepage: String? = null,
-    val id: Long? = null,
+    val id: Long,
 
     @Json(name = "imdb_id")
     val imdbID: String? = null,
 
     @Json(name = "original_language")
-    val originalLanguage: String? = null,
+    val originalLanguage: String,
 
     @Json(name = "original_title")
-    val originalTitle: String? = null,
+    val originalTitle: String,
 
     val overview: String? = null,
-    val popularity: Double? = null,
+    val popularity: Double,
 
     @Json(name = "poster_path")
     val posterPath: String? = null,
 
     @Json(name = "production_companies")
-    val productionCompanies: List<ProductionCompanyWrapper>? = null,
+    val productionCompanies: List<ProductionCompanyWrapper>,
 
     @Json(name = "production_countries")
-    val productionCountries: List<ProductionCountryWrapper>? = null,
+    val productionCountries: List<ProductionCountryWrapper>,
 
     @Json(name = "release_date")
-    val releaseDate: String? = null,
+    val releaseDate: String,
 
-    val revenue: Long? = null,
+    val revenue: Long,
     val runtime: Long? = null,
 
     @Json(name = "spoken_languages")
-    val spokenLanguages: List<SpokenLanguageWrapper>? = null,
+    val spokenLanguages: List<SpokenLanguageWrapper>,
 
-    val status: String? = null,
+    val status: String,
     val tagline: String? = null,
-    val title: String? = null,
-    val video: Boolean? = null,
+    val title: String,
+    val video: Boolean,
 
     @Json(name = "vote_average")
-    val voteAverage: Double? = null,
+    val voteAverage: Double,
 
     @Json(name = "vote_count")
-    val voteCount: Long? = null,
+    val voteCount: Long,
 
     var isFavorite: Boolean? = false
 )
 
 @JsonClass(generateAdapter = true)
 data class ProductionCompanyWrapper (
-    val id: Long? = null,
+    val id: Long,
 
     @Json(name = "logo_path")
     val logoPath: String? = null,
 
-    val name: String? = null,
+    val name: String,
 
     @Json(name = "origin_country")
-    val originCountry: String? = null
+    val originCountry: String
 )
 @JsonClass(generateAdapter = true)
 data class ProductionCountryWrapper (
@@ -89,7 +87,7 @@ data class ProductionCountryWrapper (
 @JsonClass(generateAdapter = true)
 data class SpokenLanguageWrapper (
     @Json(name = "iso_639_1")
-    val iso639_1: String? = null,
+    val iso639_1: String,
 
-    val name: String? = null
+    val name: String
 )
