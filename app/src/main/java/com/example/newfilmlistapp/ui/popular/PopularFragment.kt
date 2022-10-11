baf6fc68e67bdd9d80f64ede82c3ea33ac8cee90
Ashawagandha-coder <max.cacz@yandex.ru>
2022-10-11 13:26:03 +0500

    delete old code

--- app/src/main/java/com/example/newfilmlistapp/ui/popular/PopularFragment.kt
+++ app/src/main/java/com/example/newfilmlistapp/ui/popular/PopularFragment.kt
@@ -7,11 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
-import androidx.paging.CombinedLoadStates
 import androidx.paging.LoadState
-import androidx.paging.LoadStates
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newfilmlistapp.view_model.PopularViewModel
 import com.example.newfilmlistapp.databinding.FragmentPopularBinding
--- app/src/main/java/com/example/newfilmlistapp/view_model/FavoritesViewModel.kt
+++ app/src/main/java/com/example/newfilmlistapp/view_model/FavoritesViewModel.kt
@@ -1,27 +1,21 @@
 package com.example.newfilmlistapp.view_model
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
-import com.example.newfilmlistapp.model.MovieDetailWrapper
 import com.example.newfilmlistapp.paging.MovieFavoritePagingSource
 import com.example.newfilmlistapp.repository.Impl.ImplRepositoryRoom
 
 
-class FavoritesViewModel() : ViewModel() {
-
-    private val mutableLiveData_favorite: MutableLiveData<List<MovieDetailWrapper>> = MutableLiveData()
-    val favorite = mutableLiveData_favorite
+class FavoritesViewModel : ViewModel() {
 
 
     private val listData = Pager(PagingConfig(pageSize = 20)) {
         MovieFavoritePagingSource(ImplRepositoryRoom())
     }.flow.cachedIn(viewModelScope)
     val getListData = listData
-
 
 
 }
