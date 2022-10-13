3712d3b2801f5111053e44f850afb1f418108c42
Ashawagandha-coder <max.cacz@yandex.ru>
2022-10-13 13:59:29 +0500

    add impl

--- app/src/main/java/com/example/newfilmlistapp/repository/Impl/ImplRepositoryAPI.kt
+++ app/src/main/java/com/example/newfilmlistapp/repository/Impl/ImplRepositoryAPI.kt
@@ -1,9 +1,13 @@
 package com.example.newfilmlistapp.repository.Impl
 
 import android.util.Log
+import com.example.newfilmlistapp.model.GenresWrapper
+import com.example.newfilmlistapp.model.MovieDetailWrapper
+import com.example.newfilmlistapp.model.MovieWrapper
 import com.example.newfilmlistapp.model.PopularWrapper
 import com.example.newfilmlistapp.network.LoadingMovieDBService
 import com.example.newfilmlistapp.repository.RepositoryAPI
+import com.example.newfilmlistapp.view_model.movie_recomendation.MovieRecomendationViewModel
 import com.example.newfilmlistapp.view_model.popular.PopularViewModel
 
 class ImplRepositoryAPI: RepositoryAPI {
@@ -19,4 +23,58 @@ class ImplRepositoryAPI: RepositoryAPI {
         return result
 
     }
+
+
+    override suspend fun getMovieDetail(movieID: String): MovieDetailWrapper {
+
+        val service = LoadingMovieDBService.create()
+
+        val result = service.getMovieDetail(movieID)
+
+        Log.d(MovieRecomendationViewModel::class.java.name, "request OK - Movie Detail ")
+
+        return result
+
+    }
+
+    override suspend fun getMovie(year: Int, genre: String): MovieWrapper {
+
+        val service = LoadingMovieDBService.create()
+
+        val result = service.getMovie(primary_release_year = year, genres = listOf(genre))
+
+        Log.d(MovieRecomendationViewModel::class.java.name,"request OK in suspend - Random Movie ")
+
+        return result
+
+    }
+
+
+    override suspend fun getGenres(): GenresWrapper {
+
+        val service = LoadingMovieDBService.create()
+
+        val result = service.getGenres()
+
+        Log.d(MovieRecomendationViewModel::class.java.name,"request OK in suspend - Genres")
+
+        return result
+    }
+
+
+    override suspend fun getDefaultMovie(): MovieWrapper {
+
+        val service = LoadingMovieDBService.create()
+
+        val result = service.getDefaultMovie()
+
+        Log.d(MovieRecomendationViewModel::class.java.name,"request OK in suspend - Default Movie ")
+
+        return result
+
+
+    }
+
+
+
 }
